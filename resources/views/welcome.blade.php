0502102596587437c8732b47d50a015622a2cfb7
tassbib <tassbib@gmail.com>
2025-10-30 08:42:59 +0100

    Fix CSS build for production

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,15 @@
-<x-portfolio-layout>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Portfolio TASS</title>
+    
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <!-- Ton contenu --><x-portfolio-layout>
     <x-slot name="title">Portfolio - TASSOU YERIMA MAMA</x-slot>
     
     <!-- Header/Hero -->
@@ -13,3 +24,5 @@
     <!-- Contact -->
     @livewire('contact-form')
 </x-portfolio-layout>
+</body>
+</html>
